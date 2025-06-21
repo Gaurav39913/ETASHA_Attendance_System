a4be66bcd3cd1092adf5b18885865bbdd0efae86
Gaurav39913 <gauravgaurav48264@gmail.com>
2025-06-21 17:01:06 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,10 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'main.admin_login'
+    main = Blueprint('main', __name__)
+
+@main.route('/admin/login')
+def admin_login():
 
     @login_manager.user_loader
     def load_user(user_id):
